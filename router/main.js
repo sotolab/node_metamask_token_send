92046bbc24e08a8fba98351beec6f155afaf06ab
sotojang <mrjang28@gmail.com>
2021-06-16 21:19:08 +0900

    Update main.js

--- router/main.js
+++ router/main.js
@@ -7,11 +7,11 @@ const web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io
 //console.log("contractAbi:", contractAbi);
 
 
-const contractAddress = '0xEc2aeC198Cd49bbae4c59EA3Ff36306944785BBd'
+const contractAddress = 'yourContractAddress'
 
 const contractOwner = {
-	addr: '0x6bd3fB52291596a4cD54B1eeb872B71Bd9F12133',
-	key: '6f2ce47c6c03ed42e22a4d02ab5f980f7e916827c0a86ebc8865f2066c3c37c4'
+	addr: 'yourAccount',
+	key: 'yourPrivateKey'
 };
 
 module.exports = function (app) {
